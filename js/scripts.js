e7d43bf0050bb51f2826efa6e061eba372f965ff
ian mwangi <iankimotho233@gmail.com>
2019-11-10 01:46:54 +0300

    Add a constructor that keeps the values of krust,toppings and size

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+function pizza(the_size,the_toppings,the_krust){
+    this.pizza_size = the_size;
+    this.pizza_toppings = the_toppings;
+    this.pizza_krust = the_krust;
+}
+
 $(document).ready(function(){
     $("button").click(function(event){
         event.preventDefault();
@@ -13,7 +19,9 @@ function pizzaPrices(){
     var toppings = $("input[name='toppings']:checked").val();
     var krust = $("input[name='krust']:checked").val();
 
-    alert(size+" sized, "+toppings+" topping, "+krust+" pizza");
+    var newPizza = new pizza(size,toppings,krust);
+
+    alert(newPizza.pizza_size+" sized, "+newPizza.pizza_toppings+" topping, "+newPizza.pizza_krust+" pizza");
 
     var prices = [500,600,700,800,900,1000,650,750,850,550,650,750,850,950,1050,1000,1100,1200,950,1050,1150,450,550,650,1150,1250,1350];
     
@@ -21,6 +29,86 @@ function pizzaPrices(){
 
     if(size==="small" && toppings==="mushrooms" && krust==="crispy"){
         price = prices[0];
+    }
+    else if(size==="small" && toppings==="mushrooms" && krust==="stuffed"){
+        price = prices[1];
+    }
+    else if(size==="small" && toppings==="mushrooms" && krust==="gluten free"){
+        price = prices[2];
+    }
+    else if(size==="small" && toppings==="pineapple" && krust==="crispy"){
+        price = prices[3];
+    }
+    else if(size==="small" && toppings==="pineapple" && krust==="stuffed"){
+        price = prices[4];
+    }
+    else if(size==="small" && toppings==="pineapple" && krust==="gluten free"){
+        price = prices[5];
+    }
+    else if(size==="small" && toppings==="bacon" && krust==="crispy"){
+        price = prices[6];
+    }
+    else if(size==="small" && toppings==="bacon" && krust==="stuffed"){
+        price = prices[7];
+    }
+    else if(size==="small" && toppings==="bacon" && krust==="gluten free"){
+        price = prices[8];
+    }
+
+    else if(size==="medium" && toppings==="mushrooms" && krust==="crispy"){
+        price = prices[9];
+    }
+    else if(size==="medium" && toppings==="mushrooms" && krust==="stuffed"){
+        price = prices[10];
+    }
+    else if(size==="medium" && toppings==="mushrooms" && krust==="gluten free"){
+        price = prices[11];
+    }
+    else if(size==="medium" && toppings==="pineapple" && krust==="crispy"){
+        price = prices[12];
+    }
+    else if(size==="medium" && toppings==="pineapple" && krust==="stuffed"){
+        price = prices[13];
+    }
+    else if(size==="medium" && toppings==="pineapple" && krust==="gluten free"){
+        price = prices[14];
+    }
+    else if(size==="medium" && toppings==="bacon" && krust==="crispy"){
+        price = prices[15];
+    }
+    else if(size==="medium" && toppings==="bacon" && krust==="stuffed"){
+        price = prices[16];
+    }
+    else if(size==="medium" && toppings==="bacon" && krust==="gluten free"){
+        price = prices[17];
+    }
+
+    else if(size==="large" && toppings==="mushrooms" && krust==="crispy"){
+        price = prices[9];
+    }
+    else if(size==="large" && toppings==="mushrooms" && krust==="stuffed"){
+        price = prices[10];
+    }
+    else if(size==="large" && toppings==="mushrooms" && krust==="gluten free"){
+        price = prices[11];
+    }
+    else if(size==="large" && toppings==="pineapple" && krust==="crispy"){
+        price = prices[12];
+    }
+    else if(size==="large" && toppings==="pineapple" && krust==="stuffed"){
+        price = prices[13];
+    }
+    else if(size==="large" && toppings==="pineapple" && krust==="gluten free"){
+        price = prices[14];
+    }
+    else if(size==="large" && toppings==="bacon" && krust==="crispy"){
+        price = prices[15];
+    }
+    else if(size==="large" && toppings==="bacon" && krust==="stuffed"){
+        price = prices[16];
+    }
+    else if(size==="large" && toppings==="bacon" && krust==="gluten free"){
+        price = prices[17];
     }
 
     alert(price);
