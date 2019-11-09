c55627af416f02a191a2e406df22cb718c9b386b
ian mwangi <iankimotho233@gmail.com>
2019-11-10 01:26:37 +0300

    Add a function in js file that matches the price with type of pizza ordered

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,26 @@ $(document).ready(function(){
     $("button").click(function(event){
         event.preventDefault();
 
-        var size = $("input[name='size']:checked").val();
-        var toppings = $("input[name='toppings']:checked").val();
-        var krust = $("input[name='krust']:checked").val();
-
-        alert(size+" sized, "+toppings+" topping, "+krust+" pizza");
+        pizzaPrices();
+        
     });
     
 });
+
+function pizzaPrices(){
+    var size = $("input[name='size']:checked").val();
+    var toppings = $("input[name='toppings']:checked").val();
+    var krust = $("input[name='krust']:checked").val();
+
+    alert(size+" sized, "+toppings+" topping, "+krust+" pizza");
+
+    var prices = [500,600,700,800,900,1000,650,750,850,550,650,750,850,950,1050,1000,1100,1200,950,1050,1150,450,550,650,1150,1250,1350];
+    
+    var price;
+
+    if(size==="small" && toppings==="mushrooms" && krust==="crispy"){
+        price = prices[0];
+    }
+
+    alert(price);
+}
